93abbc73191ac38e171c8626fdc030daa59a90e1
깃Kael0595 <ants7021@naver.com>
2023-03-08 12:25:57 +0900

    java 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int age = 19;
+
+        System.out.println("당신의 나이 : " + age);
+
+        if (age >= 20 && age <= 34) {
+            System.out.println("청년입니다.");
+        }
+        else {
+            System.out.println("청년이 아닙니다.");
+        }
+
     }
 }
