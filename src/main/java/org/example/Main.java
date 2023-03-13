ad45ba8deb8431ccb4927ca61aead5de93028468
깃Kael0595 <ants7021@naver.com>
2023-03-13 13:25:39 +0900

    pra-3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,20 +4,6 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-// 100 - 1
-
-        int a = 100;
-        while (a >= 1) {
-            System.out.println(a);
-            a--;
-        }
-
-        System.out.println();
-
-        for (int i = 100; i >= 1; i--) {
-            System.out.println(i);
-        }
-
 
 
     }
